53fa641bab9757208720c264650a3135a0f67087
Fortune Ekeruo <legalmody@gmail.com>
2019-02-21 19:21:02 +0100

    implement TDD for a Person, Man, Wife and Baby classes

---
+++ test.js
@@ -0,0 +1,88 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+describe('Person', function () {
+    it('Should have a name', function () {
+        expect(false).to.be.true;
+    });
+
+    it('Should have an age', function () {
+        expect(false).to.be.true;
+    });
+
+    it('Should be able to set a name', function () {
+        expect(false).to.be.true;
+    });
+
+    it('Should be able to get a name', function () {
+        expect(false).to.be.true;
+    });
+
+    it('Should be able to set age', function () {
+        expect(false).to.be.true;
+    });
+
+    it('Should be able to get age', function () {
+        expect(false).to.be.true;
+    });
+});
+
+describe('Man', function () {
+    it('Should be able to make income', function () {
+        expect(false).to.be.true;
+    });
+
+    it('Should be able to buy himself a car', function () {
+        expect(false).to.be.true;
+    });
+
+    it('Should be able to get married', function () {
+        expect(false).to.be.true;
+    });
+
+    it('Should be able to drive wife to the hospital', function () {
+        expect(false).to.be.true;
+    });
+
+    it('Should be able to drive wife home from the hospital', function () {
+        expect(false).to.be.true;
+    });
+
+    it('Should be able to buy his wife a car', function () {
+        expect(false).to.be.true;
+    });
+    
+    it('Should be able to buy his child a car', function () {
+        expect(false).to.be.true;
+    });
+});
+
+describe('Wife', function() {
+    it('Should be able to get pregnant', function () {
+        expect(false).to.be.true;
+    });
+
+    it('Should be able to bear a child', function () {
+        expect(false).to.be.true;
+    });
+});
+
+describe('Baby', function() {
+    it('Should be able to celebrate birthdays', function() {
+        expect(false).to.be.true;
+    });
+});
+
+describe('Car', function() {
+    it('Should have a model', function() {
+        expect(false).to.be.true;
+    });
+
+    it('Should have an owner', function() {
+        expect(false).to.be.true;
+    });
+
+    it('Should be driven', function() {
+        expect(false).to.be.true;
+    });
+});
